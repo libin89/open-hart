832b505ab97412f6aa6e2c68803b0d1e968a8ad7
libin <Administrator@PC-libin.(none)>
2013-09-29 12:47:13 +0800

    v1

--- src/hart_proxy.h
+++ src/hart_proxy.h
@@ -6,12 +6,18 @@
 struct hart_proxy {
 	struct hart_driver *driver;
 	void *link;
+	void *tx_appli;
+	void *rx_appli;
+	unsigned char (*hart_process_completed_notify)(void *rx_appli);
+	unsigned char (*hart_send_request_notify)(void * tx_appli);
 	unsigned char (*hart_proxy_send_end)(void *link,
 		unsigned char *data);
 	unsigned char (*hart_proxy_data_recived)(void *link,
 		unsigned char *data,
 		unsigned int cnt);
 };
+
+/* test */
 
 extern unsigned char hart_proxy_send_rq(struct hart_proxy *proxy,
 	unsigned char *data,
